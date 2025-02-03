4a66dae9642e9bea2c9886f1677f3035fb93dd9b
wh0ohm <wararat.boonchieng@gmail.com>
2025-02-03 21:20:33 +0700

    complete

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -32,3 +32,40 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+void showData(double *a, int N , int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << a[i];
+        if((i+1)%M==0) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double *a,int N,int M){
+    for(int i = 0; i < N*M; i++){
+        a[i] = (rand()%101)*0.01;
+    }
+}
+
+void findRowSum(const double *a,double *b,int N,int M){
+    double sum = 0;
+    int h = 0;
+        for(int i = 0 ; i < N*M ; i++){
+            sum += *(a+i);
+            if(i%M == M-1){ 
+                *(b+h) = sum;
+                sum = 0;
+                h++;
+            }
+        }
+}
+
+void findColSum(const double *a,double *b,int N,int M){
+    for(int i=0;i<M;i++){
+        double sum = 0 ;
+        for(int j=0;j<N;j++){
+            sum += *(a+j*M+i) ;
+        }
+        *(b+i) = sum ;
+    }
+}
